487db3f483b34a6e9afae591021cbfcb65167a37
CPoimboeuf <claire.poimboeuf@etu.unilim.fr>
2015-02-04 15:14:36 +0100

    Pour le listage des personnes (plus modifs sur ListerVilles)

--- controllers/PersonneController.js
+++ controllers/PersonneController.js
@@ -1,19 +1,27 @@
-
 var model = require('../models/personne.js');
-  
-    
-// ////////////////////////////////////////////// L I S T E R     P E R S O N N E S 
-   
+
+// ////////////////////////////////////////////// L I S T E R     P E R S O N N E S
+
 module.exports.ListerPersonne = function(request, response){
    response.title = 'Liste des personnes';
 
-   response.render('listerPersonne', response);  
-};   
+   model.getListePersonne(function(err, result){
+     if(err){
+       // gestion de l'erreur
+       console.log(err);
+       return;
+     }
 
-// ////////////////////////////////////////////// A J O U T E R     P E R S O N N E S 
-   
+     response.listePersonne = result;
+     response.nbPersonne = result.length;
+     response.render('listerPersonne', response);
+   });
+};
+
+// ////////////////////////////////////////////// A J O U T E R     P E R S O N N E S
+
 module.exports.AjouterPersonne = function(request, response){
    response.title = 'Ajout des personnes';
 
-   response.render('ajouterPersonne', response);  
-}; 
+   response.render('ajouterPersonne', response);
+};
--- models/personne.js
+++ models/personne.js
@@ -5,7 +5,7 @@ var db = require('../configDb');
 
 /*
 * Vérifie le nom utilisateur et son mot de passe
-* 
+*
 * @param     data.login : le login de l'utilisateur
 * @param     data.pass : le mot de passe
 * @return l'identifiant de la personne si le mot de passe et le login sont bons
@@ -17,9 +17,9 @@ module.exports.getLoginOk = function (data, callback) {
  	if(!err){
    	var sha256 = crypto.createHash("sha256"); // cryptage en sha256
    	sha256.update(data.pass, "utf8");
-   	var resu = sha256.digest("base64");	
-	//console.log ('Mot de passe en clair : ' + data.pass); 
-	//console.log ('Mot de passe crypté : ' + resu);	 	
+   	var resu = sha256.digest("base64");
+	//console.log ('Mot de passe en clair : ' + data.pass);
+	//console.log ('Mot de passe crypté : ' + resu);
  		req= "SELECT per_num from personne where per_login =" + connexion.escape(data.login) + " and per_pwd = " +connexion.escape(resu);
    //console.log(req);
    	connexion.query(req, callback);
@@ -28,3 +28,21 @@ module.exports.getLoginOk = function (data, callback) {
    	});
 };
 
+/*
+* Récupérer l'intégralité des personnes
+* @return Un tableau de personnes avec le numéro, le nom et le prénom
+*/
+module.exports.getListePersonne = function (callback) {
+	// connection à la base
+	db.getConnection(function(err, connexion){
+		if(!err){
+			// s'il n'y a pas d'erreur de connexion
+			// execution de la requête SQL
+			// il est conseillé de passer la requête dans une variable
+			connexion.query('SELECT per_num, per_nom, per_prenom from personne ORDER BY per_nom, per_prenom ASC', callback);
+
+			// la connexion retourne dans le pool
+			connexion.release();
+		}
+	});
+};
