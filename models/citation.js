d0a14aa958785f5fe1c3044a7a0378ca83f23cb2
Mirigan <emilien.billard@gmail.com>
2015-02-04 15:28:11 +0100

    modification citation.js

--- models/citation.js
+++ models/citation.js
@@ -6,7 +6,12 @@ module.exports.getListeCitation = function (callback){
     if(!err){
       // s'il n'y a pas d'erreur de connexion
       // execution de la requête SQL
-      var req = "SELECT c.cit_num, per_nom, cit_libelle, DATE_FORMAT(cit_date, '%d/%m/%y') as cit_date, AVG(vot_valeur) as cit_moy from citation c inner join vote v on c.cit_num = v.cit_num inner join personne p on c.per_num = p.per_num where cit_valide = 1 and cit_date_valide is not null group by c.cit_num, per_nom, cit_libelle, cit_date ";
+      var req = "SELECT c.cit_num, per_nom, cit_libelle, DATE_FORMAT(cit_date, '%d/%m/%y') as cit_date, AVG(vot_valeur) as cit_moy"
+      req += " from citation c"
+      req += " inner join vote v on c.cit_num = v.cit_num"
+      req += " inner join personne p on c.per_num = p.per_num"
+      req += " where cit_valide = 1 and cit_date_valide is not null"
+      req += " group by c.cit_num, per_nom, cit_libelle, cit_date ";
       connexion.query(req, callback);
 
       //la connexion retourne dans le pool
