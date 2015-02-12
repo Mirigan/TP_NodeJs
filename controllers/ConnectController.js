e22c61da765116c972c8ed8861f76ac66ae25b1d
Mirigan <emilien.billard@gmail.com>
2015-02-12 09:05:13 +0100

    connexion

--- controllers/ConnectController.js
+++ controllers/ConnectController.js
@@ -1,14 +1,19 @@
-
+var model = require('../models/personne.js');
 
   // ////////////////////////////////////////////// C O N N E C T   U T I L I S A T E U R
 module.exports.Connect = function(request, response){
 
-		
-
     response.render('connect', response);
 };
 
+module.exports.VerifConnect = function(request, response){
 
+	response.title = 'Vérification du mot de passe';
+
+	
+
+	response.render('connect', response);
+};
 
 
 
