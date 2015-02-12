2990d90ee13810f1d57011bd7c14aa6298d7e895
Mirigan <emilien.billard@gmail.com>
2015-02-12 08:33:55 +0100

    modification connect et personne.js

--- controllers/ConnectController.js
+++ controllers/ConnectController.js
@@ -1,15 +1,36 @@
 
 
-  // ////////////////////////////////////////////// C O N N E C T   U T I L I S A T E U R 
+  // ////////////////////////////////////////////// C O N N E C T   U T I L I S A T E U R
 module.exports.Connect = function(request, response){
-	
+
+		
+
     response.render('connect', response);
 };
 
- // ////////////////////////////////////////////// D E C O N N E C T   U T I L I S A T E U R 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // ////////////////////////////////////////////// D E C O N N E C T   U T I L I S A T E U R
 module.exports.Deconnect = function(request, response){
-	 
+
 	 response.redirect('/connect');
 };
-
-
