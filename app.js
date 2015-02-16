bcec4b776a91cae46c8bf1dcd8b11c2d354a9441
Billard Emilien <emilien.billard@gmail.com>
2015-02-16 17:43:33 +0100

    modification de app.js

--- app.js
+++ app.js
@@ -25,26 +25,31 @@ app.use(session({
     saveUninitialized: true
 }));
 
- 
+/* ces lignes permettent d'utiliser directement les variables de session dans handlebars
+UTILISATION : {{session.MaVariable}} */
+app.use(function(request, response, next){
+  response.locals.session = request.session;
+  next();
+});
+
 /* express-handlebars - https://github.com/ericf/express-handlebars
 *  Handlebars : moteur de template pour Express.
 * il va gérer les vues
 */
 hbs = handlebars.create({
    defaultLayout: 'main', // nom de la page par defaut ici main.handlebars (structure de base HTML)
-   
+
    partialsDir: ['views/partials/'] // le vues partielles (le code HTML qui se répète dans toutes les pages)
    // les vues qui changent suivant le choix de l'utilisateur sont à la racine du répertoire : views
 });
- 
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // chargement du routeur
-require('./router/router')(app); 
+require('./router/router')(app);
 
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Serveur Node.js en attente sur le port ' + app.get('port'));
 });
-
