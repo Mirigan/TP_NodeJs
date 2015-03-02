a25411b4f4266728b8c3b5300bfee95289b99dde
Billard Emilien <emilien.billard@gmail.com>
2015-03-02 11:29:38 +0100

    petite modification

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express         = require('express'),
-    session         = require('express-session'),
-    cookieParser    = require('cookie-parser'),
-    bodyParser      = require('body-parser'), //pour récupérer les résultats des post
-	 handlebars  	  = require('express-handlebars'), hbs,
-	 http = require('http'),
-	 path = require('path');
+var express = require('express'),
+    session = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'), //pour récupérer les résultats des post
+      handlebars = require('express-handlebars'), hbs,
+      http = require('http'),
+      path = require('path');
 
 var app = express();
 
@@ -13,16 +13,15 @@ app.set('port', 6800);
 app.set('views', path.join(__dirname, 'views'));
 
 // routes static, le routeur n'y aura pas accès
-app.use('/image',express.static(path.join(__dirname+ '/public/image')));
-app.use('/css',express.static(path.join(__dirname+'/public/css')));
+app.use(express.static(path.join(__dirname+ '/public')));
 
 app.use(cookieParser());
 
 app.use(session({
-    secret: 'nC0@#1pM/-0qA1+é',
-    name: 'Betisier',
-    resave: true,
-    saveUninitialized: true
+  secret: 'nC0@#1pM/-0qA1+é',
+  name: 'Betisier',
+  resave: true,
+  saveUninitialized: true
 }));
 
 /* ces lignes permettent d'utiliser directement les variables de session dans handlebars
@@ -33,14 +32,14 @@ app.use(function(request, response, next){
 });
 
 /* express-handlebars - https://github.com/ericf/express-handlebars
-*  Handlebars : moteur de template pour Express.
+* Handlebars : moteur de template pour Express.
 * il va gérer les vues
 */
 hbs = handlebars.create({
-   defaultLayout: 'main', // nom de la page par defaut ici main.handlebars (structure de base HTML)
+  defaultLayout: 'main', // nom de la page par defaut ici main.handlebars (structure de base HTML)
 
-   partialsDir: ['views/partials/'] // le vues partielles (le code HTML qui se répète dans toutes les pages)
-   // les vues qui changent suivant le choix de l'utilisateur sont à la racine du répertoire : views
+  partialsDir: ['views/partials/'] // le vues partielles (le code HTML qui se répète dans toutes les pages)
+  // les vues qui changent suivant le choix de l'utilisateur sont à la racine du répertoire : views
 });
 
 app.engine('handlebars', hbs.engine);
@@ -48,7 +47,6 @@ app.set('view engine', 'handlebars');
 
 // chargement du routeur
 require('./router/router')(app);
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Serveur Node.js en attente sur le port ' + app.get('port'));
